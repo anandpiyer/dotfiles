32e06187af04564636afb614ecfd3b705345db62
Anand <api@cs.berkeley.edu>
2017-08-24 16:34:35 -0700

    hydra

---
+++ emacs.d/lisp/setup-hydra.el
@@ -0,0 +1,49 @@
+;;; setup-hydra.el --- Hydra
+;;
+;;; Commentary:
+;;
+;; Hydra related settings
+;;
+;;; Code:
+(use-package hydra
+  :defer t)
+
+(defhydra api/themes-hydra (:hint nil :color pink :columns 3)
+  "
+Themes
+
+^Gruvbox^   ^Material^   ^Other^
+----------------------------------------------------
+_g_: Dark     _m_: Dark    _z_: Zenburn
+_G_: Light    _M_: Light   _s_: Seoul256
+"
+  ("g" (load-theme 'gruvbox-dark-soft  t))
+  ("G" (load-theme 'gruvbox-light t))
+  ("m" (load-theme 'material        t))
+  ("M" (load-theme 'material-light  t))
+  ("z" (load-theme 'zenburn         t))
+  ("s" (load-theme 'seoul256 t))
+  ("RET" nil "done" :color blue))
+
+(use-package multiple-cursors
+  :defer t)
+
+(defhydra api/multiple-cursors-hydra (:hint nil)
+  "
+     ^Up^            ^Down^        ^Miscellaneous^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_q_] Quit"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("q" nil))
+
+(provide 'setup-hydra)
+;;; setup-hydra.el ends here
